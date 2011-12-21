d1552135a51365347cccd4339aa1d7085bf82a68
Luís Eduardo Jason Santos <jasonsantos@gmail.com>
2011-12-21 00:00:27 -0200

    Added isolated scope to basic engine test

--- testsuite/engine/test.lua
+++ testsuite/engine/test.lua
@@ -7,10 +7,12 @@ assert(capoeira._VERSION =='0.1')
 assert(capoeira._COPYRIGHT == "Copyright (C) 2010 Capoeira Project")
 assert(capoeira._DESCRIPTION =='Capoeira is a full-feature graphic application development engine and framework')
 
-local engine = capoeira:new()
+do
+	local engine = capoeira:new()
 
-assert(engine._NAME == capoeira._NAME)
-assert(engine._VERSION == capoeira._VERSION)
-assert(engine._DESCRIPTION == capoeira._DESCRIPTION)
+	assert(engine._NAME == capoeira._NAME)
+	assert(engine._VERSION == capoeira._VERSION)
+	assert(engine._DESCRIPTION == capoeira._DESCRIPTION)
+end
 
 print'OK'
