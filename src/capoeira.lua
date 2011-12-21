7ee48cc129b273f839d9cbd65bdd510fe9b7e871
Luís Eduardo Jason Santos <jasonsantos@gmail.com>
2011-12-21 00:27:31 -0200

    Implemented engine ID generation

--- src/capoeira.lua
+++ src/capoeira.lua
@@ -1,12 +1,15 @@
 local Capoeira={}
 
+local _LASTID = 0
+
 Capoeira._NAME = "Capoeira"
 Capoeira._VERSION = "0.1"
 Capoeira._COPYRIGHT = "Copyright (C) 2011 Capoeira Project"
 Capoeira._DESCRIPTION = "Capoeira is a full-feature graphic application development engine and framework"
 
 function Capoeira:new() 
-	return setmetatable({}, {
+	_LASTID = _LASTID + 1
+	return setmetatable({_ID=_LASTID}, {
 		__index=Capoeira,
 		__call=Capoeira.new
 	})
