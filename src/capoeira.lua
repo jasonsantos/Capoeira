b2fd3e2cabe26ea23ae1909ce0be71e197d83c7f
Luís Eduardo Jason Santos <jasonsantos@gmail.com>
2011-12-21 00:17:53 -0200

    Fixed year in Copyright message

--- src/capoeira.lua
+++ src/capoeira.lua
@@ -2,7 +2,7 @@ local Capoeira={}
 
 Capoeira._NAME = "Capoeira"
 Capoeira._VERSION = "0.1"
-Capoeira._COPYRIGHT = "Copyright (C) 2010 Capoeira Project"
+Capoeira._COPYRIGHT = "Copyright (C) 2011 Capoeira Project"
 Capoeira._DESCRIPTION = "Capoeira is a full-feature graphic application development engine and framework"
 
 function Capoeira:new() 
--- testsuite/engine/test.lua
+++ testsuite/engine/test.lua
@@ -4,7 +4,7 @@ local capoeira = require'capoeira'
 
 assert(capoeira._NAME =='Capoeira')
 assert(capoeira._VERSION =='0.1')
-assert(capoeira._COPYRIGHT == "Copyright (C) 2010 Capoeira Project")
+assert(capoeira._COPYRIGHT == "Copyright (C) 2011 Capoeira Project")
 assert(capoeira._DESCRIPTION =='Capoeira is a full-feature graphic application development engine and framework')
 
 do
