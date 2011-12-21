a8bb7a4cb4c1c6c81d3fc8ae4298a1ea81bd8357
Luís Eduardo Jason Santos <jasonsantos@gmail.com>
2011-12-21 00:56:46 -0200

    Implements engine arbitrary ID

--- src/capoeira.lua
+++ src/capoeira.lua
@@ -7,9 +7,10 @@ Capoeira._VERSION = "0.1"
 Capoeira._COPYRIGHT = "Copyright (C) 2011 Capoeira Project"
 Capoeira._DESCRIPTION = "Capoeira is a full-feature graphic application development engine and framework"
 
-function Capoeira:new() 
+function Capoeira:new(id) 
 	_LASTID = _LASTID + 1
-	return setmetatable({_ID=_LASTID}, {
+	local _id = id or _LASTID
+	return setmetatable({_ID=_id}, {
 		__index=Capoeira,
 		__call=Capoeira.new
 	})
