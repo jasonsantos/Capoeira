e9c5e8aa013fe155ff175ea8f06ec1fa9fee8550
Luís Eduardo Jason Santos <jasonsantos@gmail.com>
2011-12-20 23:55:52 -0200

    Added basic module engine and version information

---
+++ src/capoeira.lua
@@ -0,0 +1,15 @@
+local Capoeira={}
+
+Capoeira._NAME = "Capoeira"
+Capoeira._VERSION = "0.1"
+Capoeira._COPYRIGHT = "Copyright (C) 2010 Capoeira Project"
+Capoeira._DESCRIPTION = "Capoeira is a full-feature graphic application development engine and framework"
+
+function Capoeira:new() 
+	return setmetatable({}, {
+		__index=Capoeira,
+		__call=Capoeira.new
+	})
+end
+
+return setmetatable(Capoeira, {__call=Capoeira.new})
